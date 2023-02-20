3cabe391bc31bdd7ae788ad4b05b96aa7c462777
kumilachew-g <kumilachew1212@gmail.com>
2023-02-20 18:02:28 +0300

    Update xml

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
+
+import { toast, ToastContainer } from "react-toastify";
 import { lowerCaseLetters, numbers } from "./components/Character";
 import { copyFail, copySuccess } from "./components/message";
 const App = () => {
@@ -9,17 +11,6 @@ const App = () => {
   const [includeLowerCase, setIncludeLowerCase] = useState(false);
   const [includeUpperCase, setIncludeUpperCase] = useState(false);
   const [includeSymbols, setIncludeSymbols] = useState(false);
-
-  const createPassword = (characterList) => {
-    let password = "";
-    const characterListLength = characterList.length;
-    for (let i = 0; i < passwordLength; i++) {
-      const characterIndex = Math.round(Math.random() * characterListLength);
-      password = password + characterList.charAt(characterIndex);
-    }
-    return password;
-  };
-
   const handleGeneratePassword = () => {
     if (
       !includeLowerCase &&
@@ -48,10 +39,43 @@ const App = () => {
     }
   };
 
+  const createPassword = (characterList) => {
+    let password = "";
+    const characterListLength = characterList.length;
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength);
+      password = password + characterList.charAt(characterIndex);
+    }
+    return password;
+  };
+
   const copyToClipboard = (password) => {
     navigator.clipboard.writeText(password);
   };
 
+  const notify = (message, hasError = false) => {
+    if (hasError) {
+      toast.error(message, {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      toast(message, {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
   const handleCopyPassword = (e) => {
     if (password === "") {
       notify(copyFail, true);
@@ -62,9 +86,29 @@ const App = () => {
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <p> Hello World! </p>
-      </header>
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator_header">Password Generator</h2>
+          <div className="generator_password">
+            <h3>{password}</h3>
+            <button className="copy__btn">
+              <i onClick={handleCopyPassword} className="fas fa-copy"></i>
+            </button>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-strength"> Password length </label>
+            <input
+              type="number"
+              name="password-strength"
+              id="password-strength"
+              max="26"
+              min="8"
+              defaultValue={passwordLength}
+              onChange={(e) => setPasswordLength(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
