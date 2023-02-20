655ffa4c39ed8b9a42b5fbbda811a595f3d497f4
kumilachew-g <kumilachew1212@gmail.com>
2023-02-20 16:53:02 +0300

    Inicialize states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import Raect, { useState } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [Password, setPassword] = useState("");
+  const [passwordLength, setPasswordLength] = useState(26);
+  const [includenumbers, setIncludeNumbers] = useState(false);
+  const [includeLowerCase, setIncludeLowerCase] = useState(false);
+  const [includeUpperCase, setIncludeUpperCase] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -8,6 +15,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
